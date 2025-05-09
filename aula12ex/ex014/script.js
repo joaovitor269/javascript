e3c9c195a242072860864268dc98b098279f7e6a
João Vitor T. <joaovitorteixeira269@gmail.com>
2025-05-09 01:20:41 -0300

    ex 2 resolvido

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,22 +2,21 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 11
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
-        img.src = 'fotomanha.png'
+        img.src = 'fotomanha2.jpg'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
         // BOA TARDE!
-        img.src = 'fototarde.png'
-        img.width = 500
-        img.height = 250
+        img.src = 'fototarde2.jpg'
+        document.body.style.background = '#e8a76f'
+        
     } else {
         // BOA NOITE!
-        img.src = 'fotonoite.png'
-        img.width = 350
-        img.height = 250
+        img.src = 'fotonoite2.jpg'
+        document.body.style.background = '#232929'
     }
 }
  
