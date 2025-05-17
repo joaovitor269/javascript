59e3423ce26da2e995f9ccff79ee29af6ed48e7b
João Vitor T. <joaovitorteixeira269@gmail.com>
2025-05-17 19:20:24 -0300

    fiz o ex na aula agr

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,45 +1,33 @@
-function contando() {
-    var res = document.querySelector('div#res')
-    var val = document.getElementsByName('ini')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    var inicio = Number(val[0].value)
-    var fim = Number(val[1].value)
-    var passo = Number(val[2].value)
-
-    if (val[0].value.length == 0 || val[1].value.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossivel contar!'
-    } else if (passo <= 0) {
-
-        alert('Passo invalidado! Considerando PASSO 1')
-        passo = 1
-        res.innerHTML = 'Contando: </br>'
-        if (inicio < fim) {
-            // Contagem Crescente
-            for(var c = inicio; c <= fim; c+= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
-            }
-        } else if (fim < inicio) {
-            // Contagem Decrescente
-            for(var c = inicio; c >= fim; c-= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
-            }
-        }
-        res.innerHTML += `\u{1F3C1}` // Bandeira de Chegada
-        
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        
-        res.innerHTML = 'Contando: </br>'
-        if (inicio < fim) {
-            // Contagem Crescente
-            for(var c = inicio; c <= fim; c+= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
-        } else if (fim < inicio) {
-            // Contagem Decrescente
-            for(var c = inicio; c >= fim; c-= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
         }
-        res.innerHTML += `\u{1F3C1}` // Bandeira de Chegada
+        res.innerHTML += `\u{1F3C1}`
+        
     }
 }
---
+++ desafios/des03/ex003-js/script.js
@@ -0,0 +1,45 @@
+function contando() {
+    var res = document.querySelector('div#res')
+    var val = document.getElementsByName('ini')
+
+    var inicio = Number(val[0].value)
+    var fim = Number(val[1].value)
+    var passo = Number(val[2].value)
+
+    if (val[0].value.length == 0 || val[1].value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+    } else if (passo <= 0) {
+
+        alert('Passo invalidado! Considerando PASSO 1')
+        passo = 1
+        res.innerHTML = 'Contando: </br>'
+        if (inicio < fim) {
+            // Contagem Crescente
+            for(var c = inicio; c <= fim; c+= passo) {
+                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+            }
+        } else if (fim < inicio) {
+            // Contagem Decrescente
+            for(var c = inicio; c >= fim; c-= passo) {
+                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` // Bandeira de Chegada
+        
+    } else {
+        
+        res.innerHTML = 'Contando: </br>'
+        if (inicio < fim) {
+            // Contagem Crescente
+            for(var c = inicio; c <= fim; c+= passo) {
+                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+            }
+        } else if (fim < inicio) {
+            // Contagem Decrescente
+            for(var c = inicio; c >= fim; c-= passo) {
+                res.innerHTML += `${c} \u{1F449}` // mão seta ->
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` // Bandeira de Chegada
+    }
+}
