986e4c53f1959c9460420fb1894086120b305927
João Vitor T. <joaovitorteixeira269@gmail.com>
2025-05-18 14:09:56 -0300

    simplifiquei  uma parte

--- desafios/des03/ex003-js/script.js
+++ desafios/des03/ex003-js/script.js
@@ -6,27 +6,13 @@ function contando() {
     var fim = Number(val[1].value)
     var passo = Number(val[2].value)
 
-    if (val[0].value.length == 0 || val[1].value.length == 0) {
+    if (val[0].value.length == 0 || val[1].value.length == 0 || val[2].value.length == 0) {
         res.innerHTML = 'Impossivel contar!'
-    } else if (passo <= 0) {
-
-        alert('Passo invalidado! Considerando PASSO 1')
-        passo = 1
-        res.innerHTML = 'Contando: </br>'
-        if (inicio < fim) {
-            // Contagem Crescente
-            for(var c = inicio; c <= fim; c+= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
-            }
-        } else if (fim < inicio) {
-            // Contagem Decrescente
-            for(var c = inicio; c >= fim; c-= passo) {
-                res.innerHTML += `${c} \u{1F449}` // mão seta ->
-            }
-        }
-        res.innerHTML += `\u{1F3C1}` // Bandeira de Chegada
-        
     } else {
+        if (passo <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            passo = 1
+        }
         
         res.innerHTML = 'Contando: </br>'
         if (inicio < fim) {
